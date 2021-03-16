d4166cbd8a1760e1caa4138f0c666e7d6b404461
nasustim <mitsuhibino@gmail.com>
2021-03-17 01:07:43 +0900

    topic/mm done

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 import firebase from "firebase";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 
+firebase.initializeApp({})
 firebase.firestore().useEmulator('localhost', 8080) // exemple
 
 function App() {
 
-  const [values, loading, error] = useCollectionData(firebase.firestore().collection("<collection-name>"), { idField: "id" })
+  const [values, loading, error] = useCollectionData(firebase.firestore().collection("<room-table-name>"), { idField: "id" })
   if (loading || typeof values === 'undefined') {
     return <div>Loading...</div>;
   }
@@ -19,7 +20,12 @@ function App() {
     <div className="App">
       <ul>
       {values.map(value => (
-        <li key={value.id}>{value}</li>
+        <React.Fragment>
+        <li key={value.id}>{value.name}</li>
+        <ul>{
+          value.member.map(({device_id}: {device_id: string}) => (<li key={device_id}>{device_id}</li>))
+        }</ul>
+        </React.Fragment>
       ))}
     </ul>
     </div>
